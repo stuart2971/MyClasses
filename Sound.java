c085680944747dde11e039c06395183ff4d3cb53
Stu <stu300@rogers.com>
2018-03-21 10:33:32 -0400

    Add files via upload

--- Sound.java
+++ Sound.java
@@ -7,17 +7,26 @@ class Sound
 {
 	String source;
 	
+	//play sound from path: Sound.playSound("path/sound.wav");
 	public static void playSound(String file)
 	{
 		try{
 			String soundName = file;
+			if(soundName.charAt(soundName.length() - 4) != '.')
+			{
+				soundName = soundName + ".wav";
+			}
 			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(soundName).getAbsoluteFile());
 			Clip clip = AudioSystem.getClip();
 			clip.open(audioInputStream);
 			clip.start();
-		}catch(Exception e){}
+		}catch(Exception e){
+			System.out.println(e);
+		}
 	}
 	
+	//constructor for sound object: Sound sound= new Sound("path/sound.wav")
+	//use with sound.play()
 	Sound(String _source)
 	{
 		source = _source;
@@ -26,5 +35,25 @@ class Sound
 	public void play()
 	{
 		playSound(source);
+	}
+	
+	//sound directory object: Sound.SoundDir dir = new Sound.SoundDir("path");
+	//use with dir.playSound("sound.wav")
+	public static class SoundDir
+	{
+	  String path;
+		
+	  SoundDir(String _path)
+		{
+	    path = _path;
+			if(path.charAt(path.length() - 1) != '/')
+			{
+				path = path + "/";
+			}
+	  }
+		
+	  public void playSound(String file){
+	    Sound.playSound(path + file);
+	  }
 	}
 }
