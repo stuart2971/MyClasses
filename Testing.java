91c9473a66e815af90d2f3e4f3993ddeba69f05e
Stuart Fong <stuartbfong@gmail.com>
2018-03-22 12:39:28 -0400

    Update Testing.java

--- Testing.java
+++ Testing.java
@@ -10,20 +10,6 @@ public class Testing{
       }
     });
   }
-  static JRadioButton[] radios = {
-    new JRadioButton("True"),
-    new JRadioButton("False")
-  };
-
-  static JComponent[] objects = {
-    new JLabel("Hello"),
-    new JButton("Hi"),
-    new JTextField(),
-    radios[0],
-    radios[1]
-  };
-  static MyButtonGroup group = new MyButtonGroup(radios);
-  static MyPanel MainPanel = new MyPanel(objects, Color.RED, new GridLayout(4, 1));
 
   public static void guiApp(){
     MyFrame frame = new MyFrame(600, 400, MainPanel, "This is my frame", null);
